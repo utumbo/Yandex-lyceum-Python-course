ef22ab5bd7275bf01b3d4d515c1f99672a49e638
utumbo <121032782+utumbo@users.noreply.github.com>
2025-07-14 20:30:28 +0700

    create if - elif - else

---
+++ Level 0. Ввод, вывод, переменные/Step 3. Условный оператор/Гигрометр/gigrometr.py
@@ -0,0 +1,8 @@
+n = int(input())
+
+if n >= 30 and n <= 70:
+    print("Нормально")
+elif n < 30:
+    print("Сухо")
+else:
+    print("Влажно")
---
+++ Level 0. Ввод, вывод, переменные/Step 3. Условный оператор/Не туда/not.py
@@ -0,0 +1,6 @@
+direction = input()
+
+if direction == "налево":
+    print("Туда")
+else:
+    print("Не туда")
