a2b94599b3fb6b7b5b6d28496fb2bdd3da094f90
utumbo <121032782+utumbo@users.noreply.github.com>
2025-07-26 12:00:55 +0700

    //

--- Level 0. Ввод, вывод, переменные/Step 4. Операции со строками. Оператор in/По возрастанию/upper.py
+++ Level 0. Ввод, вывод, переменные/Step 4. Операции со строками. Оператор in/По возрастанию/upper.py
@@ -1,10 +1,18 @@
 str1 = input()
 str2 = input()
 
-if str1> str2:
+if str1[0].isupper()> str2[0].isupper():
     print(f'{str1}, {str2}')
-else:
+elif str2[0].isupper() > str1[0].isupper():
     print(f'{str2}, {str1}')
+elif str1[0]()> str2[0]:
+    print(f'{str1}, {str2}')
+elif str2[0] > str1[0]:
+    print(f'{str2}, {str1}')
+elif str1 > str2:
+    print(f'{str1}, {str2}')
+elif str2 > str1:
+    print(f'{str2} , {str1}')
 
 """
 Выведите две введённые строки в лексикографическом порядке через запятую и пробел.
