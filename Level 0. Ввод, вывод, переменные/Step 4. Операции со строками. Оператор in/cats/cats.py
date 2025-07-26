0d022fe2ad6919c1643177cd05121c44dd4c20f3
utumbo <121032782+utumbo@users.noreply.github.com>
2025-07-26 12:11:55 +0700

    //

---
+++ Level 0. Ввод, вывод, переменные/Step 4. Операции со строками. Оператор in/cats/cats.py
@@ -0,0 +1,6 @@
+cats_name = input()
+
+if 'Bar'in cats_name or 'Mur' in cats_name and 'llo' not in cats_name:
+    print('Подходит')
+else:
+    print('Неподходит')
--- Level 0. Ввод, вывод, переменные/Step 4. Операции со строками. Оператор in/По возрастанию/upper.py
+++ Level 0. Ввод, вывод, переменные/Step 4. Операции со строками. Оператор in/По возрастанию/upper.py
@@ -1,18 +1,9 @@
 str1 = input()
 str2 = input()
 
-if str1[0].isupper()> str2[0].isupper():
-    print(f'{str1}, {str2}')
-elif str2[0].isupper() > str1[0].isupper():
-    print(f'{str2}, {str1}')
-elif str1[0]()> str2[0]:
-    print(f'{str1}, {str2}')
-elif str2[0] > str1[0]:
-    print(f'{str2}, {str1}')
-elif str1 > str2:
-    print(f'{str1}, {str2}')
-elif str2 > str1:
-    print(f'{str2} , {str1}')
+result = sorted([str1, str2])
+
+print(f'{result[0]}, {result[1]}')
 
 """
 Выведите две введённые строки в лексикографическом порядке через запятую и пробел.
