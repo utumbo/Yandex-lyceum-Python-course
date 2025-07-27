badb394d8b8e6d743b36969be9cb48910ff227c6
utumbo <121032782+utumbo@users.noreply.github.com>
2025-07-28 02:25:01 +0700

    create test task

---
+++ Level 0. Ввод, вывод, переменные/Контрольная работа/Аквариум/aquarium.py
@@ -0,0 +1,14 @@
+length = int(input())
+height = int(input())
+width = int(input())
+botls = int(input())
+
+cube = length * height * width
+
+result = cube // botls
+print(result)
+
+"""
+В новый аквариум нужно налить чистой воды. Напишите программу, которая определит,
+ сколько бутылок воды нужно купить, чтобы заполнить аквариум полностью.
+"""
