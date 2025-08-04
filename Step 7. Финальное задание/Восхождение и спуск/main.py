375ab1f4a8c863c3faf0505e9e422fb8a67fee28
utumbo <121032782+utumbo@users.noreply.github.com>
2025-08-04 21:00:31 +0700

    end create final

--- Step 7. Финальное задание/Восхождение и спуск/main.py
+++ Step 7. Финальное задание/Восхождение и спуск/main.py
@@ -4,15 +4,19 @@ step = int(input())
 
 heights = []
 
-while initial_height <= height_mountain:
-    heights.append(initial_height)
-    initial_height += step
+current_height = initial_height
+while current_height <= height_mountain:
+    heights.append(current_height)
+    current_height += step
 
-current_height = height_mountain
-while current_height > initial_height:
+current_height = height_mountain - (step + 1)
+while current_height >= initial_height:
+    heights.append(current_height)
     current_height -= (step + 1)
-    if current_height >= initial_height:  
-        heights.append(current_height)
+
+
+if heights[-1] != initial_height:
+    heights.append(initial_height)
 
 print(" ".join(map(str, heights)))
 
