2f333e75d8c3f75d5bdbc0e266418f9065fcddf3
utumbo <121032782+utumbo@users.noreply.github.com>
2025-08-07 23:25:48 +0700

    update

--- Level 2. Коллекции/Полупалиндром/main.py
+++ Level 2. Коллекции/Полупалиндром/main.py
@@ -1,12 +1,20 @@
-txt = input()
+def find_half_palindrome_index(s):
+    left = 0
+    right = len(s) - 1
+    
+    while left < right:
+        if s[left] == s[right]:
+            left += 1
+            right -= 1
+        else:
+            return left
+    
+    return len(s)
 
-s1 = txt[: len(txt) // 2]
-s2 = txt[len(txt) // 2 :]
-res = ''
+input_string = input().strip()
+index = find_half_palindrome_index(input_string)
+print(index)
 
-
-
-print(res)
 """
 Палиндром — это строка, которая читается одинаково с начала и с конца. 
 А вот если сперва строка похожа на палиндром, но, начиная с какой-то буквы, 
